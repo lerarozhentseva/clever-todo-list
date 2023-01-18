9d69c7b405c073db9e15a762a76d757457a56175
Valeriya <lerarozhentseva@gmail.com>
2023-01-18 20:19:58 +0300

    refactor eslint

--- my-app/src/app/App.jsx
+++ my-app/src/app/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { LoginPage } from "../pages/LoginPage/LoginPage";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import {LoginPage} from "../pages/LoginPage/LoginPage";
 import RegistrationPage from "../pages/RegistrationPage/RegistrationPage";
 import HomePage from "../pages/HomePage/HomePage";
 import TodoPage from "../pages/TodoPage/TodoPage";
@@ -10,10 +10,10 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} exact />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegistrationPage />} />
-        <Route path="/todolist" element={<TodoPage />} />
+        <Route path="/" element={<HomePage/>} exact/>
+        <Route path="/login" element={<LoginPage/>}/>
+        <Route path="/register" element={<RegistrationPage/>}/>
+        <Route path="/todolist" element={<TodoPage/>}/>
       </Routes>
     </Router>
   );
